8a308db06378d8b87f37e1fe5d86dd2b88ced05f
ethanothepro <72050359+ethanothepro@users.noreply.github.com>
2023-09-17 00:21:48 -0700

    newuser linnk adds user to db

--- frontend/src/NewUser.js
+++ frontend/src/NewUser.js
@@ -1,11 +1,15 @@
 import { useHistory } from "react-router-dom/cjs/react-router-dom";
 import useFetch from "./useFetch";
 import { v4 as uuidv4 } from 'uuid';
+import { useEffect, useState } from "react";
 
 const NewUser = ({ user, isAuthenticated, isLoading }) => {
 
     const history = useHistory();
-    
+    const [isRegistered, setIsRegistered] = useState(false);
+    const [isFirstTime, setIsFirstTime] = useState(false);
+
+
     //if anonymous user tries to access this page, route them back to home
     if (!isLoading && !isAuthenticated){
         history.push('/')
@@ -13,29 +17,56 @@ const NewUser = ({ user, isAuthenticated, isLoading }) => {
 
     const uniqueId = user && user.sub
 
-    //Step 2: Check if user exist in db, if so redirect them to home page
-
-    //Attempt to grab data
-    
+ 
     const userUrl = 'https://radiant-gorge-79799-b57d03ac0ddd.herokuapp.com/api/users/' + uniqueId
-
-    if (uniqueId != null){
-        fetch(userUrl, {
+    
+    //useEffect to only run once when component mounts
+    useEffect(() => {
+        if (uniqueId != null) {
+          console.log(uniqueId);
+          fetch(userUrl, {
             method: 'GET',
-        }).then(res => {
-               
-            if(!res.ok){
-                throw Error('Could not fetch the data for that resource');
-            }
-            return res.json();
-        }).then(data => {
-            console.log(data)
-        })}
+          })
+            .then(res => {
+
+              if (res.ok){
+                
+                setIsRegistered(true);
+              }
+
+                //if user doesnt exist, create new user
+              if (!res.ok) {
+                fetch('https://radiant-gorge-79799-b57d03ac0ddd.herokuapp.com/api/users', {
+                    method: 'POST',
+                    headers: { "Content-Type": "application/json"},
+                    body: JSON.stringify({
+                        "user_id":  user.sub,
+                        "username" : user.nickname,
+                        "user_picture" : user.picture
+                    })
+                    }).then(()=>{
+                        //do something else here before routing to main
+                        setIsFirstTime(true);
+                        
+                        
+                        
+                    }) 
+              }
+              
+              return res.json();
+            })
+            .then(data => {
+              console.log(data);
+            });
+        }
+      }, [uniqueId, userUrl]);
             
     
- 
+      if (isRegistered){
+        history.push('/')
+      }
 
-    return
+  
     // let uploaded = false
     // //if doesnt exist, populate database
     // if (user && !isPending && error && !uploaded){
@@ -68,6 +99,11 @@ const NewUser = ({ user, isAuthenticated, isLoading }) => {
     return ( 
     <div>
         <h1>Loading</h1>
+        {isRegistered && <h1>Registered</h1>}
+        {isFirstTime && <h1>First Time</h1>}
+
+
+
 
     </div> );
 }
